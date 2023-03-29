c47047f99a371790f7353d6251eb99bea6b0817d
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-03-29 21:57:12 +0530

    changes

--- devpagemain/script.js
+++ devpagemain/script.js
@@ -1,4 +1,8 @@
 var objImage;
+var iconpop1;
+var iconpop2;
+var iconpop3;
+var iconpop4;
 var w =
   window.innerWidth ||
   document.documentElement.clientWidth ||
@@ -12,6 +16,10 @@ function init() {
   objImage.style.position = "absolute";
   objImage.style.left = "1vw";
   objImage.style.bottom = "11.5vh";
+  iconpop1 = document.getElementById("icon1");
+  iconpop2 = document.getElementById("icon2");
+  iconpop3 = document.getElementById("icon3");
+  iconpop4 = document.getElementById("icon4");
 }
 function getKeyAndMove(e) {
   var key_code = e.which || e.keyCode;
@@ -21,6 +29,7 @@ function getKeyAndMove(e) {
       break;
     case 38: //Up arrow key
       moveup();
+      jumping();
 
       break;
     case 39: //right arrow key
@@ -37,17 +46,13 @@ function moveLeft() {
         document.getElementById("walk1").classList.toggle("walkhidden");
         document.getElementById("walk2").classList.toggle("walkhidden");
 
-        objImage.style.left = pos - 5 + "px";
-        console.log(pos);
-        console.log(i);
+        objImage.style.left = pos - 35 + "px";
       }
       if (i % 2 != 0) {
         document.getElementById("walk2").classList.toggle("walkhiiden");
         document.getElementById("walk1").classList.toggle("walkhidden");
 
-        objImage.style.left = pos - 5 + "px";
-        console.log(pos);
-        console.log(i);
+        objImage.style.left = pos - 35 + "px";
       }
     }
   }
@@ -60,62 +65,76 @@ function moveRight() {
       if (i % 2 == 0) {
         document.getElementById("walk1").classList.toggle("walkhidden");
         document.getElementById("walk2").classList.toggle("walkhidden");
-        objImage.style.left = pos + 5 + "px";
+        objImage.style.left = pos + 25 + "px";
         objImage.style.transform = scaleX(1);
 
-        console.log(pos);
-        console.log(i);
       }
       if (i % 2 != 0) {
         document.getElementById("walk2").classList.toggle("walkhiiden");
         document.getElementById("walk1").classList.toggle("walkhidden");
-        objImage.style.left = pos + 5 + "px";
+        objImage.style.left = pos + 25 + "px";
         objImage.style.transform = scaleX(1);
 
-        console.log(pos);
-        console.log(i);
       }
     }
   }
 }
+function jumping(){
+  objImage.classList.add("jump");
+
+}
 function moveup() {
-  document.getElementById("popupfront").style.scale=0;
+  objImage.classList.remove("jump");
+  iconpop1.classList.remove("iconjump");
+  iconpop2.classList.remove("iconjump");
+  iconpop3.classList.remove("iconjump");
+  iconpop4.classList.remove("iconjump");
+  document.getElementById("popupfront").style.scale = 0;
 
-  document.getElementById("popupback").style.scale=0;
+  document.getElementById("popupback").style.scale = 0;
 
-  document.getElementById("popupvideo").style.scale=0;
+  document.getElementById("popupvideo").style.scale = 0;
 
-  document.getElementById("popupdesign").style.scale=0;
-
+  document.getElementById("popupdesign").style.scale = 0;
 
   brick1pos = document.getElementById("bricks1").offsetLeft;
   brick2pos = document.getElementById("bricks2").offsetLeft;
   brick3pos = document.getElementById("bricks3").offsetLeft;
   brick4pos = document.getElementById("bricks4").offsetLeft;
   personpos = document.getElementById("walk").offsetLeft;
-  if(personpos>=brick1pos && personpos<=brick1pos+300){
-    document.getElementById("popupfront").style.scale=1;
-    objImage.classList.toggle("jump");
+  brick1pos = document.getElementById("bricks1").offsetLeft;
+  brick1width = document.getElementById("bricks1").offsetWidth;
+  brick2width = document.getElementById("bricks2").offsetWidth;
+  brick3width = document.getElementById("bricks3").offsetWidth;
+  brick4width = document.getElementById("bricks4").offsetWidth;
+  console.log(personpos);
+  console.log(brick1pos);
+  console.log(brick1width);
 
+
+
+  if (personpos >= brick1pos && personpos <= brick1pos + brick1width) {
+    objImage.classList.add("jump");
+    document.getElementById("popupfront").style.scale = 1;
+    iconpop1.classList.add("iconjump");
+    
   }
-  if(personpos>=brick2pos && personpos<=brick2pos+300){
-    document.getElementById("popupback").style.scale=1;
-    objImage.classList.toggle("jump");
-
-  }
-  if(personpos>=brick3pos && personpos<=brick3pos+300){
-    document.getElementById("popupvideo").style.scale=1;
-    objImage.classList.toggle("jump");
-
-  }
-  if(personpos>=brick4pos && personpos<=brick4pos+300){
-    document.getElementById("popupdesign").style.scale=1;
-    objImage.classList.toggle("jump");
-
+  if (personpos >= brick2pos && personpos <= brick2pos + brick2width) {
+    objImage.classList.add("jump");
+    document.getElementById("popupback").style.scale = 1;
+    iconpop2.classList.add("iconjump");
   }
 
-
-
+  if (personpos >= brick3pos && personpos <= brick3pos + brick3width) {
+    objImage.classList.add("jump");
+    document.getElementById("popupvideo").style.scale = 1;
+    iconpop3.classList.add("iconjump");
+  }
+  if (personpos >= brick4pos && personpos <= brick4pos + brick4width) {
+    objImage.classList.add("jump");
+    document.getElementById("popupdesign").style.scale = 1;
+    iconpop4.classList.add("iconjump");
+  }
 }
 
 window.onload = init;
